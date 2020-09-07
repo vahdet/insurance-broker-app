e2ba71d4408991b3441c2f38c6ab51f1cd1e72dc
vahdet <vahdetkeskin@gmail.com>
2020-09-08 00:39:49 +0300

    back to regular backend ports

--- api/app/app.py
+++ api/app/app.py
@@ -69,4 +69,4 @@ def users():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=5000)
--- api/auth/app.py
+++ api/auth/app.py
@@ -152,4 +152,4 @@ def get_token() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6001)
+    app.run(host='0.0.0.0', port=5001)
